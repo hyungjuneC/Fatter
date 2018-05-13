57b9d266b9158010403877111ad351353095cc59
Kim Gyeong-Hyeon <37342126+ddamddi@users.noreply.github.com>
2018-05-13 18:58:26 +0900

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,38 @@
+
+public class Controller {
+	
+	User myUser = new User();
+	LoginModule myLoginModule = new LoginModule();
+	DBModule myDBModule = new DBModule();
+	Alarm myAlarm = new Alarm();
+	Graph myGraph = new Graph();
+	Setting mySetting = new Setting();
+	
+	public Controller() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void login() {
+		
+	}
+	
+	public void register() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void setting() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void alarm() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void showGraph() {
+		// TODO Auto-generated method stub
+
+	}
+}
