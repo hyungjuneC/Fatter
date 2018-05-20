8d8d144d1337e76904e4b0226bde36b4fe82d0b0
Sunwoo Kwon <32264161+soonwooKwon@users.noreply.github.com>
2018-05-20 19:00:11 +0900

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -9,32 +9,41 @@ public class Controller {
 	Setting mySetting = new Setting();
 	
 	public Controller() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	// 로그인 - LoginModule
-	public void login() {
 		
 	}
 	
-	// 회원가입 - LoginModule
-	public void register() {
-		// TODO Auto-generated method stub
+	public void login() { 			// 로그인
+		Scanner scn = new Scanner(System.in);
+		boolean resultOfLogin = false;		// 로그인 성공 여부 
+		do{					// 로그인 성공할때까지 사용자의 입력을 받는다. 
+			System.out.print("ID:") 		// 현재는 UI 고려하지 않음
+			String strId = scn.next();
+		
+			System.out.print("PW:") 
+			String strPw = scn.next();
+		
+			resultOfLogin = myLoginModule.compareIdPassword(strId,strPw);
+		}
+		while(resultOfLogin == false);
+	}
+	
+	public void register() {		// 회원가입 
+		myLoginModule.registerUser();
+
+	}
+		
+	public void setting() {			// 프로그램 환경 설정
+	
 
 	}
 	
-	public void setting() {
-		// TODO Auto-generated method stub
-
-	}
-	
-	public void alarm() {
-		// TODO Auto-generated method stub
+	public void alarm() {			// 알람 control
+		
 
 	}
 	
 	public void showGraph() {
-		// TODO Auto-generated method stub
-
+		myGraph.showUserWeeklyTotalNutrient();	//user의 일주일 영양상태를 그래프로 출력
+		mygraph.showUserPractice();		//user의 운동상태를 그래프로 출력
 	}
 }
