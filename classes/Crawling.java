716be5a98a430db7078008b45fd53e1da87707ce
hyungjuneC <33713418+hyungjuneC@users.noreply.github.com>
2018-06-09 23:02:53 +0900

    Add files via upload

--- classes/Crawling.java
+++ classes/Crawling.java
@@ -24,7 +24,7 @@ public class Crawling {
 	static Elements nutritions;
 	static Element a; 
 	static String url = "https://www.foodsafetykorea.go.kr/portal/healthyfoodlife/foodnutrient/simpleSearch.do?menu_no=2805&menu_grp=MENU_NEW03&code4=2&code2=&search_name=&page=";
-	static File output = new File("C:\\Users\\caucse\\Desktop\\Fastway-master\\FoodDB.txt");
+	static File output = new File("FoodDB.txt");
     
 	public static void main(String[] args) {
 		
@@ -47,13 +47,13 @@ public class Crawling {
 		         for(Element e: names) {
 		        	 count++;
 		        	 if(count>=3) {
-				        System.out.println(e.text());
-				         
+				        System.out.print(e.text());
+				        System.out.print(":");
 		        	 
-		        	 for(j = 1+count2*11; j<(11+count2*11); j++) {
+		        	 for(j = 2+count2*11; j<(6+count2*11); j++) {
 							
 							a = nutritions.get(j);
-							 if(j==(10+count2*11)) {
+							 if(j==(5+count2*11)) {
 					        	 System.out.print(a.text());
 					        	 System.out.println("");
 					         }else {
@@ -104,13 +104,14 @@ public class Crawling {
 		         for(Element e: names) {
 		        	 count++;
 		        	 if(count>=3) {
-				         fw.write(String.format("%s\r\n",e.text()));
+				         fw.write(String.format("%s",e.text()));
+				         fw.write(String.format(":"));
 				         
 		        	 
-		        	 for(j = 1+count2*11; j<(11+count2*11); j++) {
+		        	 for(j = 2+count2*11; j<(6+count2*11); j++) {
 							
 							a = nutritions.get(j);
-							 if(j==(10+count2*11)) {
+							 if(j==(5+count2*11)) {
 					        	 fw.write(String.format("%s\r\n",a.text()));
 					         }else {
 							 fw.write(String.format("%s",a.text()));
